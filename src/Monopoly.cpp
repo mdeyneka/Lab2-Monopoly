0170af0a7c0ba8a032c1adf7a5c6294192f78384
Maxim Deyneka <maxim.deyneka.ua@gmail.com>
2023-11-07 17:11:02 +0100

    Change variables to more readable in Buy and Renta methods, add comments

--- src/Monopoly.cpp
+++ src/Monopoly.cpp
@@ -36,45 +36,45 @@ std::tuple<std::string, int> Monopoly::GetPlayerInfo(int m)
 	return *i;
 }
 
-bool Monopoly::Buy(int z, std::tuple<std::string, Type, int, bool> k)
+bool Monopoly::Buy(int playerIndex, std::tuple<std::string, Type, int, bool> field)
 {
-	auto x = GetPlayerInfo(z);
-	tuple<string, int> p;
-	list<tuple<std::string, Type, int, bool>>::iterator i;
-	list<tuple<string, int>>::iterator j = Players.begin();
-	switch (get<1>(k))
+	auto foundedPlayer = GetPlayerInfo(playerIndex); // found Player's data by index
+	tuple<string, int> tempPlayer;
+	list<tuple<std::string, Type, int, bool>>::iterator fieldIterator;
+	list<tuple<string, int>>::iterator playerIterator = Players.begin();
+	switch (get<1>(field))
 	{
 	case AUTO:
-		if (get<2>(k))
-			return false;
-		p = make_tuple(get<0>(x), get<1>(x) - 500);
-		k = make_tuple(get<0>(k), get<1>(k), z, get<2>(k));
+		if (get<2>(field)) // Check the owner
+			return false; // if the owner EXIST - return
+		tempPlayer = make_tuple(get<0>(foundedPlayer), get<1>(foundedPlayer) - 500); // Copy player's info with 500 money decrease
+		field = make_tuple(get<0>(field), get<1>(field), playerIndex, get<2>(field)); // Copy field's info and set player index owner, flag 'already owned' as true
 		break;
 	case FOOD:
-		if (get<2>(k))
+		if (get<2>(field))
 			return false;
-		p = make_tuple(get<0>(x), get<1>(x) - 250);
-		k = make_tuple(get<0>(k), get<1>(k), z, get<2>(k));
+		tempPlayer = make_tuple(get<0>(foundedPlayer), get<1>(foundedPlayer) - 250);
+		field = make_tuple(get<0>(field), get<1>(field), playerIndex, get<2>(field));
 		break;
 	case TRAVEL:
-		if (get<2>(k))
+		if (get<2>(field))
 			return false;
-		p = make_tuple(get<0>(x), get<1>(x) - 700);
-		k = make_tuple(get<0>(k), get<1>(k), z, get<2>(k));
+		tempPlayer = make_tuple(get<0>(foundedPlayer), get<1>(foundedPlayer) - 700);
+		field = make_tuple(get<0>(field), get<1>(field), playerIndex, get<2>(field));
 		break;
 	case CLOTHER:
-		if (get<2>(k))
+		if (get<2>(field))
 			return false;
-		p = make_tuple(get<0>(x), get<1>(x) - 100);
-		k = make_tuple(get<0>(k), get<1>(k), z, get<2>(k));
+		tempPlayer = make_tuple(get<0>(foundedPlayer), get<1>(foundedPlayer) - 100);
+		field = make_tuple(get<0>(field), get<1>(field), playerIndex, get<2>(field));
 		break;
 	default:
 		return false;
 	};
-	i = find_if(Fields.begin(), Fields.end(), [k](auto x) { return get<0>(x) == get<0>(k); });
-	*i = k;
-    advance(j, z-1);
-	*j = p;
+	fieldIterator = find_if(Fields.begin(), Fields.end(), [field](auto foundedPlayer) { return get<0>(foundedPlayer) == get<0>(field); }); //find field in Monopoly list
+	*fieldIterator = field; //set as a new field with updated parameters
+    advance(playerIterator, playerIndex-1); //move iterator according to index
+	*playerIterator = tempPlayer; //set as a new player with updated parameters
 	return true;
 }
 
@@ -86,51 +86,53 @@ std::tuple<std::string, Monopoly::Type, int, bool>  Monopoly::GetFieldByName(std
 	return *i;
 }
 
-bool Monopoly::Renta(int m, std::tuple<std::string, Type, int, bool> c)
+bool Monopoly::Renta(int playerIndex, std::tuple<std::string, Type, int, bool> field)
 {
-	tuple<string, int> z = GetPlayerInfo(m);
-	tuple<string, int> o;
+	tuple<string, int> foundedPlayer = GetPlayerInfo(playerIndex); // found Player's data by index
+	tuple<string, int> tempPlayer;
 
-	switch (get<1>(c))
+	switch (get<1>(field)) // Check type of field
 	{
 	case AUTO:
-		if (!get<2>(c))
-			return false;
-		o = GetPlayerInfo(get<2>(c));
-		o = make_tuple(get<0>(o), get<1>(o) + 250);
-		z = make_tuple(get<0>(z), get<1>(z) - 250);
+		if (!get<2>(field)) // Check the owner
+			return false;	// if the owner NOT EXIST - return
+		tempPlayer = GetPlayerInfo(get<2>(field)); //get owner of field
+		tempPlayer = make_tuple(get<0>(tempPlayer), get<1>(tempPlayer) + 250); // add 250 money to owner of field
+		foundedPlayer = make_tuple(get<0>(foundedPlayer), get<1>(foundedPlayer) - 250); // decrease 250 money from player 
 		break;
 	case FOOD:
-		if (!get<2>(c))
+		if (!get<2>(field))
 			return false;
 	case TRAVEL:
-		if (!get<2>(c))
+		if (!get<2>(field))
 			return false;
-		o = GetPlayerInfo(get<2>(c));
-		o = make_tuple(get<0>(o), get<1>(o) + 250);
-		z = make_tuple(get<0>(z), get<1>(z) - 250);
+		tempPlayer = GetPlayerInfo(get<2>(field));
+		tempPlayer = make_tuple(get<0>(tempPlayer), get<1>(tempPlayer) + 250);
+		foundedPlayer = make_tuple(get<0>(foundedPlayer), get<1>(foundedPlayer) - 250);
 		break;
 	case CLOTHER:
-		if (!get<2>(c))
+		if (!get<2>(field))
 			return false;
-		o = GetPlayerInfo(get<2>(c));
-		o = make_tuple(get<0>(o), get<1>(o) + 250);
-		z = make_tuple(get<0>(z), get<1>(z) - 250);
+		tempPlayer = GetPlayerInfo(get<2>(field));
+		tempPlayer = make_tuple(get<0>(tempPlayer), get<1>(tempPlayer) + 250);
+		foundedPlayer = make_tuple(get<0>(foundedPlayer), get<1>(foundedPlayer) - 250);
 		break;
 	case PRISON:
-		z = make_tuple(get<0>(z), get<1>(z) - 1000);
+		foundedPlayer = make_tuple(get<0>(foundedPlayer), get<1>(foundedPlayer) - 1000);
 		break;
 	case BANK:
-		z = make_tuple(get<0>(z), get<1>(z) - 700);
+		foundedPlayer = make_tuple(get<0>(foundedPlayer), get<1>(foundedPlayer) - 700);
 		break;
 	default:
 		return false;
 	}
+
+	/* Update both players (one with increased money, another one with decreased) */
 	list<tuple<string, int>>::iterator i = Players.begin();
-	advance(i, m - 1);
-	*i = z;
-	i = find_if(Players.begin(), Players.end(), [o](auto x) { return get<0>(x) == get<0>(o); });
-	*i = o;
+	advance(i, playerIndex - 1);
+	*i = foundedPlayer;
+	i = find_if(Players.begin(), Players.end(), [tempPlayer](auto x) { return get<0>(x) == get<0>(tempPlayer); });
+	*i = tempPlayer;
 	return true;
 }
 
